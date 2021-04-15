cb2550a4258fcc837b7c8173aca7348ae20e6127
nikunj2001 <nikunj.gupta_cs19@gla.ac.in>
2021-04-15 15:33:24 +0530

    inintial

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app =  express();
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+const PORT =  process.env.PORT || 3000;
+
+
+// Set template engine
+
+
+
+// app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs');
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+
+
+
+
+
+
+app.listen(
+PORT,(err)=>{
+    console.log(`listening on port ${PORT}`);
+}
+)
